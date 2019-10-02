d80cc48a2081e22756430e10386beece91baaf39
AntoineMaurin <antoine.maurin26@gmail.com>
2019-10-02 13:57:56 +0200

    Avancement de l'orienté objet, et prototype avec pygames fonctionnel

--- tests_pygame.py
+++ tests_pygame.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import time
 
 from labyrinthe import here_is_the_maze, display_maze
 
@@ -14,43 +15,37 @@ grey = (35, 35, 35)
 green = (0, 153, 76)
 blue = (0, 128, 255)
 
-while True:
-    OBJ1 = random.randint(1, 13), random.randint(1, 13)
-    OBJ2 = random.randint(1, 13), random.randint(1, 13)
-    OBJ3 = random.randint(1, 13), random.randint(1, 13)
-    if (THE_MAZE[OBJ1[0]][OBJ1[1]] == ' ') and (THE_MAZE[OBJ2[0]][OBJ2[1]] == ' ') and (THE_MAZE[OBJ3[0]][OBJ3[1]] == ' '):
-        THE_MAZE[OBJ1[0]][OBJ1[1]] = 'OBJ1'
-        THE_MAZE[OBJ2[0]][OBJ2[1]] = 'OBJ2'
-        THE_MAZE[OBJ3[0]][OBJ3[1]] = 'OBJ3'
-        break
-
 window.fill((240, 240, 240))
+
+def check_key():
+    if event.key == pygame.K_LEFT:
+        return 'ARROW LEFT'
+    elif event.key == pygame.K_RIGHT:
+        return 'ARROW RIGHT'
+    elif event.key == pygame.K_UP:
+        return 'ARROW UP'
+    elif event.key == pygame.K_DOWN:
+        return 'ARROW DOWN'
+
+def move_with(pressed_key):
+    if pressed_key == 'ARROW LEFT':
+        pygame.draw.rect(window, grey, pygame.Rect(0, 100, 40, 40), 2)
+    elif pressed_key == 'ARROW RIGHT':
+        pygame.draw.rect(window, grey, pygame.Rect(200, 100, 40, 40), 2)
+    elif pressed_key == 'ARROW UP':
+        pygame.draw.rect(window, grey, pygame.Rect(100, 0, 40, 40), 2)
+    elif pressed_key == 'ARROW DOWN':
+        pygame.draw.rect(window, grey, pygame.Rect(100, 200, 40, 40), 2)
+
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN :
+            pressed_key = check_key()
+            move_with(pressed_key)
 
-    x, y = 0, 0
-    w = 40
-
-    for row in THE_MAZE:
-        for cell in row:
-            if cell == 'X':
-                pygame.draw.rect(window, grey, pygame.Rect(x, y, w, w))
-            elif cell == 'MC' :
-                pygame.draw.rect(window, blue, pygame.Rect(x, y, w, w))
-            elif cell == 'OBJ1' :
-                pygame.draw.rect(window, green, pygame.Rect(x, y, w, w))
-            elif cell == 'OBJ2' :
-                pygame.draw.rect(window, green, pygame.Rect(x, y, w, w))
-            elif cell == 'OBJ3' :
-                pygame.draw.rect(window, green, pygame.Rect(x, y, w, w))
-            else:
-                pygame.draw.rect(window, grey, pygame.Rect(x, y, w, w), 2)
-            x = x + w
-        x = 0
-        y = y + w
 
     pygame.display.flip()
